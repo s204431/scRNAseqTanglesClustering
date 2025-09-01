1fa9ed8ad17e831aa2ad983e3d154fb3fb8d15ec
s204431 <s204431@student.dtu.dk>
2025-09-01 11:41:51 +0200

    Added initial python file and data

---
+++ scRNAseq.py
@@ -0,0 +1,44 @@
+import scanpy as sc
+import anndata as ad
+import pandas as pd
+
+sc.settings.set_figure_params(dpi=50, facecolor="white")
+
+df = pd.read_csv("data/symsim_observed_counts_5000genes_1000cells_complex.csv", index_col=0)
+adata = ad.AnnData(df)
+
+sc.pp.filter_cells(adata, min_genes=100)
+sc.pp.filter_genes(adata, min_cells=3)
+
+# Saving count data
+adata.layers["counts"] = adata.X.copy()
+# Normalizing to median total counts
+sc.pp.normalize_total(adata)
+# Logarithmize the data
+sc.pp.log1p(adata)
+
+sc.pp.highly_variable_genes(adata, n_top_genes=2000)
+
+sc.tl.pca(adata)
+
+
+sc.pp.neighbors(adata)
+sc.tl.umap(adata)
+
+# Using the igraph implementation and a fixed number of iterations can be significantly faster, especially for larger datasets
+sc.tl.leiden(adata, flavor="igraph", n_iterations=2)
+
+sc.pl.umap(adata, color=["leiden"])
+
+
+
+labels = pd.read_csv("data/symsim_labels_5000genes_1000cells_complex.csv", index_col=0)
+
+adata.obs["ground_truth"] = labels.iloc[:, 0].values.astype(str)  
+adata.obs["ground_truth"]
+
+sc.pl.umap(adata, color=["ground_truth"])
+
+
+
+clusters = adata.obs["leiden"]
