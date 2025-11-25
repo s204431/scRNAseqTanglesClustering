a507fe600d7f31f93d233f670c0ca76f76a05f7b
s195901 <s195901@student.dtu.dk>
2025-11-25 15:36:12 +0100

    Scanpy changes

--- scRNAseq.py
+++ scRNAseq.py
@@ -7,6 +7,7 @@ import json
 import time
 import numpy as np
 from sklearn.metrics import silhouette_score, davies_bouldin_score
+from kneed import KneeLocator
 
 
 RUN_FROM_JAVA = True
@@ -31,7 +32,15 @@ while not RUN_FROM_JAVA or int(sys.stdin.readline().strip()) == 1:
     else:
         df = pd.read_csv(input_str, index_col=0)
         adata = ad.AnnData(df)
+        
     
+    # Shuffle
+    perm = np.random.permutation(adata.n_obs)
+    adata.uns["_perm"] = perm
+    adata._inplace_subset_obs(perm)
+
+    
+        
     start_time = time.time()
     
     #sc.pp.filter_cells(adata, min_genes=100)
@@ -50,7 +59,7 @@ while not RUN_FROM_JAVA or int(sys.stdin.readline().strip()) == 1:
     
     SEED = np.random.randint(0, 100000)
     
-    n_pcs = 50
+    n_pcs = 100
     if use_pca: sc.tl.pca(adata, n_comps=n_pcs)
     else: sc.tl.tsne(adata, n_pcs=n_pcs, random_state=SEED)
     
@@ -81,17 +90,26 @@ while not RUN_FROM_JAVA or int(sys.stdin.readline().strip()) == 1:
         k = int(np.argmax(distances)) + 1  # +1 because PCs are 1-based conceptually
         return k
     
+    def choose_pcs_by_kneedle(vr):
+        x = np.arange(1, len(vr) + 1)
+        kneedle = KneeLocator(x, vr, curve='convex', direction='decreasing')
+        k = kneedle.knee
+        if k is None:
+            k = len(vr)
+        return k
+    
     if use_pca:
         # Per-PC explained variance
         vr = adata.uns['pca']['variance_ratio']
-        n_elbow = choose_pcs_by_elbow(vr)
+        #n_elbow = choose_pcs_by_elbow(vr)
+        n_elbow = choose_pcs_by_kneedle(vr)
     
     
     """
     # Quick visualization of "elbow"
     plt.figure(figsize=(5,3))
     plt.plot(np.arange(1, len(vr)+1), vr, marker='o')
-    plt.axvline(k_elbow, ls='--', color='gray')
+    plt.axvline(n_elbow, ls='--', color='gray')
     plt.xlabel("PC")
     plt.ylabel("Explained variance ratio")
     plt.title("PCA elbow")
@@ -112,14 +130,14 @@ while not RUN_FROM_JAVA or int(sys.stdin.readline().strip()) == 1:
     use_rep = "X_pca" if use_pca else "X_tsne"
     X_k = adata.obsm[use_rep][:, :n_pcs]
     if use_tuning: #Tune parameters
-        for n_neighbors in np.arange(5, 50, 5):
+        for n_neighbors in np.arange(5, 55, 5):
             
             if use_pca:
                 sc.pp.neighbors(adata, n_neighbors=n_neighbors, n_pcs=n_elbow, use_rep=use_rep, random_state=SEED)
             else:
                 sc.pp.neighbors(adata, n_neighbors=n_neighbors, use_rep=use_rep, random_state=SEED)
             
-            for resolution in np.arange(0.1, 2.1, 0.1):
+            for resolution in np.arange(0.1, 3.2, 0.1):
                 # Using the igraph implementation and a fixed number of iterations can be significantly faster, especially for larger datasets
                 sc.tl.leiden(adata, flavor="igraph", n_iterations=-1, resolution=resolution, random_state=SEED)
                 if len(np.unique(adata.obs["leiden"])) >= 2:
@@ -144,6 +162,14 @@ while not RUN_FROM_JAVA or int(sys.stdin.readline().strip()) == 1:
     
         
     #sc.pl.umap(adata, color=["leiden"])
+    
+    # Unshuffle
+    perm = adata.uns["_perm"]
+    inv_perm = np.argsort(perm)
+    clusters = clusters.iloc[inv_perm].reset_index(drop=True)
+
+
+
     
     end_time = time.time()
     #clusters = adata.obs["leiden"]
